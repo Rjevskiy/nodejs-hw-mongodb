5027f97c58c9329d54ab8905e761d24a66d47e60
Alexsander Rjevskiy <porezkakir2@gmail.com>
2025-03-03 00:17:05 +0200

    ф

--- src/index.js
+++ src/index.js
@@ -9,18 +9,18 @@ const PORT = process.env.PORT || 3000;
 
 // Логирование значений переменных окружения
 console.log('MONGODB_USER:', process.env.MONGODB_USER);
-console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
+console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD ? '******' : 'Not set'); // Скрываем пароль
 console.log('MONGODB_URL:', process.env.MONGODB_URL);
 console.log('MONGODB_DB:', process.env.MONGODB_DB);
+console.log('PORT:', PORT);
 
-// Соединение с MongoDB
 const mongoURI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
    .then(() => {
-      console.log("MongoDB connected");
-      startServer(); // Если подключение успешно, запускаем сервер
+      console.log("🔥 MongoDB connected");
+      startServer(PORT); // Если подключение успешно, запускаем сервер на указанном порту
    })
    .catch(err => {
-      console.error('MongoDB connection error: ', err.message);
+      console.error('❌ MongoDB connection error:', err.message);
    });
