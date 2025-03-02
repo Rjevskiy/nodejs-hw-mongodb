95b69133234ca7dd01760973804620d9c049c048
Alexsander Rjevskiy <porezkakir2@gmail.com>
2025-03-02 23:30:52 +0200

    ф

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { startServer } from './server.js'; // Импорт функции запуска сервера
 import { initMongoDB } from './db/initMongoConnection.js'; // Импорт функции подключения к MongoDB
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Устанавливаем соединение с MongoDB перед запуском сервера
-initMongoDB()
-  .then(() => {
-    startServer();  // Если соединение с БД успешно, запускаем сервер
-  })
-  .catch((error) => {
-    console.error('Error while connecting to MongoDB:', error.message);
-  });
-
-  const mongoose = require('mongoose');
+// Соединение с MongoDB
 const mongoURI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-   .then(() => console.log("MongoDB connected"))
-   .catch(err => console.log("MongoDB connection error: ", err));
+   .then(() => {
+      console.log("MongoDB connected");
+      startServer(); // Если подключение успешно, запускаем сервер
+   })
+   .catch(err => {
+      console.error('MongoDB connection error: ', err.message);
+   });
