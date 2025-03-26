62780757f8c3c8e096adc01bb9c06d1ce38795d8
Alexsander Rjevskiy <porezkakir2@gmail.com>
2025-03-26 22:58:46 +0200

    1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import authRouter from './routes/auth.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 
-dotenv.config();
+dotenv.config({ path: '.env' });
 
 
 //console.log(process.env);
