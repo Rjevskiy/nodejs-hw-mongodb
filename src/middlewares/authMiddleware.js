6b1236feaff7dd8e4108d9f27e18d5862e5eb0af
Alexsander Rjevskiy <porezkakir2@gmail.com>
2025-03-27 22:08:17 +0200

    ы

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import createHttpError from "http-errors";
+
+export const authenticate = (req, res, next) => {
+  try {
+    const { accessToken } = req.cookies;
+
+    if (!accessToken) {
+      throw createHttpError(401, "Unauthorized: No token provided");
+    }
+
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded; // Добавляем пользователя в `req` для дальнейшего использования
+
+    next();
+  } catch (error) {
+    next(createHttpError(401, "Unauthorized: Invalid token"));
+  }
+};
