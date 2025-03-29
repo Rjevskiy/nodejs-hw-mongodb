76a8ac2e1f66722ac34a1d16e6b934d92b2ae16a
Alexsander Rjevskiy <porezkakir2@gmail.com>
2025-03-29 21:59:07 +0200

    ок

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -1,30 +1,30 @@
 import Joi from 'joi';
 
-//  регистрация
+//  реєстрація
 export const registerSchema = Joi.object({
   name: Joi.string().min(3).max(30).required().messages({
-    'string.min': 'Имя должно быть не менее 3 символов',
-    'string.max': 'Имя должно быть не более 30 символов',
-    'any.required': 'Поле "name" обязательно для заполнения',
+    'string.min': "Ім'я повинно містити не менше 3 символів",
+    'string.max': "Ім'я повинно містити не більше 30 символів",
+    'any.required': 'Поле "name" є обов’язковим для заповнення',
   }),
   email: Joi.string().email().required().messages({
-    'string.email': 'Некорректный формат email',
-    'any.required': 'Поле "email" обязательно для заполнения',
+    'string.email': 'Некоректний формат email',
+    'any.required': 'Поле "email" є обов’язковим для заповнення',
   }),
   password: Joi.string().min(6).required().messages({
-    'string.min': 'Пароль должен содержать не менее 6 символов',
-    'any.required': 'Поле "password" обязательно для заполнения',
+    'string.min': 'Пароль повинен містити не менше 6 символів',
+    'any.required': 'Поле "password" є обов’язковим для заповнення',
   }),
 });
 
-//  логин
+//  вхід
 export const loginSchema = Joi.object({
   email: Joi.string().email().required().messages({
-    'string.email': 'Некорректный формат email',
-    'any.required': 'Поле "email" обязательно для заполнения',
+    'string.email': 'Некоректний формат email',
+    'any.required': 'Поле "email" є обов’язковим для заповнення',
   }),
   password: Joi.string().min(6).required().messages({
-    'string.min': 'Пароль должен содержать не менее 6 символов',
-    'any.required': 'Поле "password" обязательно для заполнения',
+    'string.min': 'Пароль повинен містити не менше 6 символів',
+    'any.required': 'Поле "password" є обов’язковим для заповнення',
   }),
 });
